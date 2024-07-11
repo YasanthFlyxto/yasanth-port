d5aa02fabf92a0c31a0aa094d0d757c32928a8c2
YasanthFlyxto <143312439+YasanthFlyxto@users.noreply.github.com>
2024-07-11 10:22:41 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/xban',
+        destination: 'https://second-sphynx-984.notion.site/b06b5ffe2ff448018ee934034b791796?v=f3e1f07e6dcb420bae8610d45cc9249e', // Replace with the actual URL you want to redirect to
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
